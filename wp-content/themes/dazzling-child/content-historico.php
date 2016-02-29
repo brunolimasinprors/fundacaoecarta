fa37819db88d39e7a2993da263f5f242a1dd6c00
leoduarte1 <leonardo.silva@sinprors.org.br>
2016-02-29 14:24:05 -0300

    Bug do historico

--- wp-content/themes/dazzling-child/content-historico.php
+++ wp-content/themes/dazzling-child/content-historico.php
@@ -98,53 +98,7 @@ if ($query->have_posts()) {
 
 unset($query);
 unset($arrAnosPublicados);
-?>
-<!--
-            <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-                
-                
-                <div class="panel panel-default">
-                    <div class="panel-heading" role="tab" id="headingOne">
-                        <h4 class="panel-title">
-                            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                Collapsible Group Item #1
-                            </a>
-                        </div>
-                        <div id="collapse<?= $anoPublicacao; ?>" class="panel-collapse collapse <?= ($count == 0) ? "in" : ""; ?>" role="tabpanel" aria-labelledby="heading<?= $anoPublicacao; ?>">
-                            <div class="panel-body espacamento-corpo-painel-historico-eventos caixa-dropdown-galeria" >
-                                <div class="row">
-                                    <?php
-                                    while ($query->have_posts()) {
-                                        $query->the_post();
-                                        $anoFim = null;
-                                        $anoFim = date('Y', strtotime(get_field("data_fim_area_2")));
-                                        if ($anoFim == $anoPublicacao) {
-                                            /* =======================================================================================================	
-                                            '* Exibe Conteúdo do histórico de eventos
-                                            '======================================================================================================== */
-                                            include(get_template_directory() . "-child/includes/conteudo-historico.php");
-                                        } //if ($anoFim == $anoPublicacao) {
-                                        unset($anoFim); //evitar erros de sujeira na memória
-                                    } //while ($query->have_posts()) {
-                                    ?>
-                                </div>        
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
-                <?php
-                $ultimoAno = $anoPublicacao;
-            } //if ($anoPublicacao !== $ultimoAno) {
-            $count++; //controlar o primeiro registro
-        } //foreach ($arrAnosPublicados as $anoPublicacao) {
-    }else{
-        echo "Nenhum histórico de evento encontrado.";
-    } //if ($query->have_posts()) {
-
-    unset($query);
-    unset($arrAnosPublicados);
-    
     //> Restaura os dados originais do post
     wp_reset_postdata();
 ?>
