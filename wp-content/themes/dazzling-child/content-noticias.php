19261073128fec1eeabc6f1bc96e1efda97fe45d
Lucas Emerim Marques <lucas.emerim@sinprors.org.br>
2016-03-04 18:58:13 -0300

    Publicação plugin ordenação de categorias e tags baixar o banco de dados
    Publicação plugin ordenação de categorias e tags baixar o banco de dados

--- wp-content/themes/dazzling-child/content-noticias.php
+++ wp-content/themes/dazzling-child/content-noticias.php
@@ -1,62 +1,68 @@
 <?php
 
-include(get_template_directory() . "-child/includes/complementar.php");
-/* =================================================================================================================	
-  '* Retorna a relação de posts pertencentes a <<categoria principal acessada>>
-  '================================================================================================================= */
-$idPagina = retornaPaginaAcessada(retornaUrlAtual());
-if (empty($idPagina)) {
-    $idPagina = 1;
-}
+    include(get_template_directory() . "-child/includes/complementar.php");
+    /* =================================================================================================================	
+      '* Retorna a relação de posts pertencentes a <<categoria principal acessada>>
+      '================================================================================================================= */
+    $idPagina = retornaPaginaAcessada(retornaUrlAtual());
+    if (empty($idPagina)) {
+        $idPagina = 1;
+    }
 
 
-$args = array(
-    'post_type' => 'post', //> Tipo de post <area2_post_type>
-    'post_status' => 'publish', //> Exibir post com a situação publicado	
-    'cat' => $idCategoriaPrincipalAcessada, //> Id categoria principal acessada
-    'tag_id' => $idTagAcessada,
-    'posts_per_page' => TOTAL_POSTS_POR_PAGINA,
-    'paged' => $idPagina,
-    'orderby' => 'date',
-    'order' => 'DESC'
-);
+    $args = array(
+        'post_type' => 'post', //> Tipo de post <area2_post_type>
+        'post_status' => 'publish', //> Exibir post com a situação publicado	
+        'cat' => $idCategoriaPrincipalAcessada, //> Id categoria principal acessada
+        'tag_id' => $idTagAcessada,
+        'posts_per_page' => TOTAL_POSTS_POR_PAGINA,
+        'paged' => $idPagina,
+        'orderby' => 'date',
+        'order' => 'DESC'
+    );
 
 
-$query = new WP_Query($args);
+    $query = new WP_Query($args);
 
-$Imagens = null;
-$Conteudo = null;
-$objImagem = null;
-$count = 0;
+    $Imagens = null;
+    $Conteudo = null;
+    $objImagem = null;
+    $count = 0;
 
-$idCategoriaPrincipal = retornaIdCategoriaPrincipalPost($post->ID);
-$aryDadosCategoria = retornaLayoutCategoriaPrincipal($idCategoriaPrincipal);
-while ($query->have_posts()) {
-    $query->the_post();
-    /* =======================================================================================================	
-      '* Exibe Conteúdo agenda
-      '======================================================================================================== */
-    include(get_template_directory() . "-child/includes/conteudo-noticias.php");
-    $count ++;
-}
+    $idCategoriaPrincipal = retornaIdCategoriaPrincipalPost($post->ID);
+    $aryDadosCategoria = retornaLayoutCategoriaPrincipal($idCategoriaPrincipal);
 
+    if (!$query->have_posts()) {    
+        echo "Nenhuma notícia cadastrada."
+    }
 
-
-$totalPaginas = $query->max_num_pages; // need an unlikely integer
-//echo str_replace( $big, '%#%', esc_url(get_pagenum_link($big)));
-$return = paginate_links(
-        array(
-            'base' => str_replace($totalPaginas, '%#%', esc_url(get_pagenum_link($totalPaginas))), //> Define formato da url
-            'format' => '?paged=%#%',
-            'current' => max(1, $idPagina),
-            'prev_next' => false,
-            'type' => 'list',
-            'total' => $query->max_num_pages
-        )
-);
+    while ($query->have_posts()) {
+        $query->the_post();
+        /* =======================================================================================================	
+          '* Exibe Conteúdo agenda
+          '======================================================================================================== */
+        include(get_template_directory() . "-child/includes/conteudo-noticias.php");
+        $count ++;
+    }
 
 
-echo str_replace("<ul class='page-numbers'>", '<ul class="page-numbers menu-paginas-noticias">', $return);
+
+    $totalPaginas = $query->max_num_pages; // need an unlikely integer
+    //echo str_replace( $big, '%#%', esc_url(get_pagenum_link($big)));
+    $return = paginate_links(
+            array(
+                'base' => str_replace($totalPaginas, '%#%', esc_url(get_pagenum_link($totalPaginas))), //> Define formato da url
+                'format' => '?paged=%#%',
+                'current' => max(1, $idPagina),
+                'prev_next' => false,
+                'type' => 'list',
+                'total' => $query->max_num_pages
+            )
+    );
+
+
+    echo str_replace("<ul class='page-numbers'>", '<ul class="page-numbers menu-paginas-noticias">', $return);
+
 ?>
 
 
